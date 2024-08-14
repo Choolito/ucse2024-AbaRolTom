c3811ccb13753a8452a8eb4b47510b6b07323406
Mateo Tomas <99299338+MateoTomas0912@users.noreply.github.com>
2024-08-14 16:16:24 -0300

    register

--- prode_project/prode_project/urls.py
+++ prode_project/prode_project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.http import HttpResponse
+from django.urls import include, path
+
+def home(request):
+    return HttpResponse("¡Bienvenido a la página de inicio!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')), #url app cuentas
+    path('', home, name='home'),  # Página de inicio
 ]
