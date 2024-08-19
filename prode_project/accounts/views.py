96e84112d03b1ddefe96b2e6db902079314ff4cc
Mateo Tomas <99299338+MateoTomas0912@users.noreply.github.com>
2024-08-19 11:26:24 -0300

    fix redirect login

--- prode_project/accounts/views.py
+++ prode_project/accounts/views.py
@@ -8,7 +8,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)  # Loguea automáticamente después de registrar
-            return redirect("home")  # Redirige a la página de inicio después del registro
+            return redirect("login")  # Redirige a la página de inicio después del registro
     else:
         form = RegisterForm()
     return render(request, "accounts/register.html", {"form": form})
