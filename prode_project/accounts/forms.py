b92ef94099cee48955a2423517c025cfa62480be
Mateo Tomas <99299338+MateoTomas0912@users.noreply.github.com>
2024-08-19 10:11:32 -0300

    login y register acomodados

--- prode_project/accounts/forms.py
+++ prode_project/accounts/forms.py
@@ -2,13 +2,20 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-#este es el form de registro que usaran los usuarios
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(
+        required=True, 
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'nombre@ejemplo.com'})
+    )
 
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Usuario'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Contraseña'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirmar Contraseña'}),
+        }
 
     def save(self, commit=True):
         user = super().save(commit=False)
