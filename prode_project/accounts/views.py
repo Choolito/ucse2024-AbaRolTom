e37be35be73c364f33a3fc4252221762c14c014a
Eric Agustin Rolheiser <rolheiser89@gmail.com>
2024-08-25 13:56:22 -0300

    Cambios en register y login
    Cambios en estilos del login y register para que sigan el estilo del resto de la pagina y ademas se modifico el register.html para que muestre los errores al momento de ingresar mal los datos

--- prode_project/accounts/views.py
+++ prode_project/accounts/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib import messages
 from .forms import RegisterForm
+from django.utils.translation import gettext as _
 
 def register(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)  # Loguea automáticamente después de registrar
-            return redirect("home")  # Redirige a la página de inicio después del registro
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, _('Cuenta creada para %(username)s. Ya puedes iniciar sesión.') % {'username': username})
+            return redirect('login')
     else:
         form = RegisterForm()
-    return render(request, "accounts/register.html", {"form": form})
+    return render(request, 'accounts/register.html', {'form': form})
     
 def home(request):
     return render(request, 'home.html')
--- prode_project/prode_project/settings.py
+++ prode_project/prode_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'prode',
-    'accounts'
+    'accounts',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'prode_project.urls'
@@ -109,14 +112,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'es'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'  # Ajusta esto a tu zona horaria
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
+LANGUAGES = [
+    ('es', _('Spanish')),
+]
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
